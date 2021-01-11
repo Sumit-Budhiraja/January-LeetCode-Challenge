6884d1ca9b7bc2a02f4afa25e1e71fa6ef094d8c
Sumit Budhiraja <48406445+Sumit-Budhiraja@users.noreply.github.com>
2021-01-12 02:08:30 +0530

    Add files via upload

---
+++ Merge Sorted Array.java
@@ -0,0 +1,15 @@
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int l = nums1.length - 1;
+        m--; n--;
+        while(m >= 0 && n >= 0) {
+            if(nums1[m] > nums2[n]) 
+                nums1[l--] = nums1[m--];
+            else 
+                nums1[l--] = nums2[n--];
+        }
+        
+        while(n >= 0) 
+            nums1[l--] = nums2[n--];
+    }
+}
