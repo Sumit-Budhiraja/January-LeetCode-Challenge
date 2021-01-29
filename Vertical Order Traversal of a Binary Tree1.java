af20b9d596a292536a4f1bbdd8c0a77fc12c06aa
Sumit Budhiraja <48406445+Sumit-Budhiraja@users.noreply.github.com>
2021-01-30 00:33:28 +0530

    Add files via upload

---
+++ Vertical Order Traversal of a Binary Tree1.java
@@ -0,0 +1,56 @@
+class Solution {
+    private class Point{
+        int x;
+        int y;
+        int val;
+
+        Point(int val, int x, int y){
+            this.val= val;
+            this.x =x;
+            this.y =y;
+        }
+    }
+
+    PriorityQueue<Point> pq =new PriorityQueue<Point>((a,b) -> {
+        if(a.x !=b.x){
+            return a.x - b.x; // lower x
+        } else{
+            if(a.y!=b.y){
+                return b.y -a.y;// higher y
+            } else{
+                return a.val - b.val; // lower val
+            }
+        }
+    });
+
+
+    public List<List<Integer>> verticalTraversal(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        treeTraversal(root, 0, 0);
+        Integer prevXIndex = null;
+        List<Integer> subArray = new ArrayList<>();
+        while(!pq.isEmpty()){
+            Point head = pq.poll();
+            if(prevXIndex!= null && head.x!=prevXIndex){
+                res.add(subArray);
+                subArray =new ArrayList<>();
+            }
+            subArray.add(head.val);
+            prevXIndex = head.x;
+        }
+        if(subArray.size()>0){
+            res.add(subArray);
+        }
+
+        return res;
+    }
+
+    private void treeTraversal(TreeNode root, int x, int y){
+        if(root == null){
+            return;
+        }
+        pq.offer(new Point(root.val, x, y));
+        treeTraversal(root.left, x-1, y-1);
+        treeTraversal(root.right, x+1, y-1);
+    }
+}
